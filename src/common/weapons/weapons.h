09d68ca1189f40afcb6cbc75fa2a9f3eb6d617bd
Cocosushi6 <sacha.tournes.savry@gmail.com>
2017-03-24 18:23:50 +0100

    added weapons.h file

---
+++ src/common/weapons/weapons.h
@@ -0,0 +1,14 @@
+#ifndef SRC_WEAPON_H define SRC_WEAPON_H include <iostream> include 
+#<string> include <vector>
+namespace Weapons {
+    
+    class Weapon {
+        public:
+            Weapon(Objects::Entities::Entity *carrier);
+            void use();
+        private:
+            Objects::Entities::Entity *carrier;
+    }
+    
+}
+#endif /* SRC_WEAPON_H */
